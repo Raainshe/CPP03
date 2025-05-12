7d73e048f1e3ae2d35fcceef33f83db4e7a99f73
Raain <ryanbwgt@gmail.com>
2025-05-12 10:44:53 +0200

    completed new functions I think

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,7 +6,55 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:27:06 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/05/12 10:27:08 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/05/12 10:43:24 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap()
+{
+    std::cout << "Default FragTrap constructor called" << std::endl;
+    this->_name = "Default";
+    this->_hp = 100;
+    this->_ep = 100;
+    this->_aDamage = 30;
+}
+
+FragTrap::FragTrap(const std::string& name)
+{
+    std::cout << "FragTrap constructor called" << std::endl;
+    this->_name = name;
+    this->_hp = 100;
+    this->_ep = 100;
+    this->_aDamage = 30;
+}
+
+FragTrap::FragTrap(const FragTrap& src) : ClapTrap(src)
+{
+    std::cout << "FragTrap copy constructor called" << std::endl;
+    *this = src;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& rhs)
+{
+    std::cout << "FragTrap assignment operator called" << std::endl;
+    if (this != &rhs)
+    {
+        this->_name = rhs._name;
+        this->_hp = rhs._hp;
+        this->_ep = rhs._ep;
+        this->_aDamage = rhs._aDamage;
+    }
+    return *this;
+}
+
+FragTrap::~FragTrap()
+{
+    std::cout << "FragTrap destructor called" << std::endl;
+}
+
+void FragTrap::highFivesGuys(void)
+{
+    std::cout << "FragTrap " << this->_name << " needs a high five :)" << std::endl;
+}
--- ex02/FragTrap.hpp
+++ ex02/FragTrap.hpp
@@ -6,7 +6,27 @@
 /*   By: rmakoni <rmakoni@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 10:27:11 by rmakoni           #+#    #+#             */
-/*   Updated: 2025/05/12 10:27:12 by rmakoni          ###   ########.fr       */
+/*   Updated: 2025/05/12 10:42:06 by rmakoni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+    public:
+        FragTrap();
+        FragTrap(const std::string& name);
+        FragTrap(const FragTrap& src);
+
+        FragTrap& operator=(const FragTrap& rhs);
+        ~FragTrap();
+        
+        void attack(const std::string& target);
+        void highFivesGuys(void);
+};
+
+#endif
